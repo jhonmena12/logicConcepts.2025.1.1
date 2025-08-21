ff8a0a67b6536ad0f681d18a4390265b4a92eefa
jhonmena12 <jhonmena20192000@gmail.com>
2025-08-21 16:08:44 -0500

    complete excercice 01

---
+++ logicConcent/isnumerOroddOrNot/Program.cs
@@ -0,0 +1,36 @@
+﻿var numberString = string.Empty;
+do
+{
+    Console.WriteLine("ingrese numero o 's' para salir: ");
+    numberString = Console.ReadLine();
+    if (numberString == "S")
+    {
+        continue;
+    }
+
+    var numberInt = 0;
+    if (int.TryParse(numberString, out numberInt))
+    {
+        if (numberInt % 2 == 0)
+        {
+            Console.WriteLine($"El numero {numberInt}, es par.");
+        }
+        else
+        {
+            Console.WriteLine($"El numero {numberInt}, es impar.");
+        }
+
+    }
+    else
+    {
+        Console.WriteLine($"lo que ingresastee {numberString}, no es un numero entero.");
+    }
+} while (numberString != "s");
+
+
+
+
+
+
+
+
